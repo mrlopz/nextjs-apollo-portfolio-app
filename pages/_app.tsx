54a9abfb0cd29d6af86897cbc44727e0bfa6e10d
LOPES_PC\leona <leonardolopesb97@hotmail.com>
2022-05-26 16:06:56 -0300

    Homepage added

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../styles/index.scss'
+
+const MyApp = ({ Component, PageProps }) => {
+  return <Component {...PageProps} />
+}
+
+export default MyApp
